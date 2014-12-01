14f748412e526d4f64c4a90197dd19fca6649acc
Matt Hebeler <matt@tacklebox.build>
2014-12-01 06:08:40 -0800

    staging static files for events page

--- staticfiles/php/eventformsettings.php
+++ staticfiles/php/eventformsettings.php
@@ -1,5 +1,5 @@
 <?php
-	$email_to = "eventcoordinator@oesrestaurant.com"; // your email address
+	$email_to = "mrhebeler@gmail.com"; // your email address
 	$email_subject = "OES Private Event Application"; // email subject line
 	$thankyou = "../thankyou.html"; // thank you page
 	// $antispam_answer = "18"; 
