641ec9d817ec08f9435a9c7a1245b961e4d138ee
Matt Hebeler <matt@tacklebox.build>
2014-12-03 04:13:58 -0800

    Trying with django php wrapper - not optimistically

--- staticfiles/php/eventformsettings.php
+++ staticfiles/php/eventformsettings.php
@@ -1,6 +1,8 @@
+{% load php %}
 <?php
 	$email_to = "mrhebeler@gmail.com"; // your email address
 	$email_subject = "OES Private Event Application"; // email subject line
 	$thankyou = "../thankyou.html"; // thank you page
 	// $antispam_answer = "18"; 
 ?>
+{% php echo 9; %}
