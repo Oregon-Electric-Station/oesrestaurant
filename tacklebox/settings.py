c07b0e2d53efb1738e8f088635ca978cb180dac4
Matt Hebeler <matt@tacklebox.build>
2014-12-10 18:28:22 -0800

    rooms page email reserver fix using rearrangement and quotations for username and password string calls

--- tacklebox/settings.py
+++ tacklebox/settings.py
@@ -107,8 +107,8 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "restaurantoes@gmail.com"
+EMAIL_HOST_PASSWORD = "Good8Food"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'restaurantoes@gmail.com'
-EMAIL_HOST_PASSWORD = 'Good8Food'
 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
